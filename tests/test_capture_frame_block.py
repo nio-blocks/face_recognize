695afcd0e700008113702ace678223475d38bdaa
cowleyk <cowley.kevin.l@gmail.com>
2017-08-22 10:35:08 -0600

    Test capture_frame_block with ipcam

--- tests/test_capture_frame_block.py
+++ tests/test_capture_frame_block.py
@@ -31,4 +31,27 @@ class TestCaptureFrame(NIOBlockTestCase):
             }))
             blk.stop()
 
-        # Does this also need to test when self.ipcam() = True?
+    def test_capture_frame_ipcam(self):
+        blk = CaptureFrame()
+        with patch(CaptureFrame.__module__ + '.urllib.request') as \
+                mock_urllib_request, \
+                patch(CaptureFrame.__module__ + '.cv2.imdecode') as \
+                mock_cv2_imdecode, \
+                patch(CaptureFrame.__module__ + '.numpy.fromstring') as \
+                mock_numpy_formstring, \
+                patch(CaptureFrame.__module__ + '.base64') as mock_base64:
+            mock_urllib_request.return_value.urlopen.return_value = 'mockStrea'
+            mock_cv2_imdecode.return_value = 'mockFrame'
+            mock_base64.b64encode.return_value.decode.return_value = 'mockEnco'
+
+
+            blk.start()
+            self.configure_block(blk, {
+                'ipcam': True
+            })
+            blk.process_signals([Signal({})])
+
+            self.assert_num_signals_notified(1)
+            self.assert_last_signal_notified(Signal({
+                'capture': 'mockEnco'
+            }))
